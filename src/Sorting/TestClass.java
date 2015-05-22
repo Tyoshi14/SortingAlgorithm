e3d48662cc76bca1b4852b34fb36d239fd36365b
Tyoshi14 <1528843602@qq.com>
2015-05-22 17:30:22 +0800

    Finish test integer

--- src/Sorting/TestClass.java
+++ src/Sorting/TestClass.java
@@ -2,11 +2,16 @@ package Sorting;
 
 import inputAndOutput.OutputExcel;
 
+import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Random;
+
+import org.apache.poi.ss.formula.functions.Choose;
 
 
 public class TestClass {
@@ -23,17 +28,9 @@ public class TestClass {
 		runningTime.put("ShellSort", new HashMap<Integer, ArrayList<Long>>());
 		runningTime.put("RadixSort", new HashMap<Integer, ArrayList<Long>>());
 		
-		for(int count=0;count<1;count++){
-			TestInteger(10,runningTime);
-		}
-		//测试排序算法的正确性
-		//TestSorting();
-// 将获得的运行时间时间结果打印到Excel 文件
+		HashMap<Integer, LinkedHashMap<String, Integer>> countTop= new HashMap<Integer, LinkedHashMap<String, Integer>>();
 		
 		
-		
-// 		用来打印数据 显示输出结果
-		ArrayList<ArrayList<String>> outputdata=new ArrayList<ArrayList<String>> ();
 		ArrayList<String> sortName=new ArrayList<String>();
 		sortName.add("QuickSort");
 		sortName.add("BubbleSort");
@@ -43,6 +40,18 @@ public class TestClass {
 		sortName.add("ShellSort");
 		sortName.add("RadixSort");
 		
+		
+		int frequecy=5;
+	    int area=50;
+		for(int count=0;count<frequecy;count++){
+			System.out.println("********************** "+count+" ****************************");
+			TestInteger(area,runningTime,countTop,sortName);
+		}
+		//测试排序算法的正确性
+		//TestSorting();
+
+// 将获得的运行时间时间结果打印到Excel 文件
+		ArrayList<ArrayList<String>> outputdata=new ArrayList<ArrayList<String>> ();
 		ArrayList<String> sampleSize=new ArrayList<String>();
 		sampleSize.add(" ");
 		HashMap<Integer, ArrayList<Long>> quickSortItem=runningTime.get("QuickSort");
@@ -66,6 +75,8 @@ public class TestClass {
 				 long totalTime=0;
 				 for (Long subTime : timeVlueList) {
 					totalTime+=subTime;
+					// 全部打印，测试输出值和平均值的正确性
+					//timeSerious.add(String.valueOf(subTime));
 				 }
 				 long average=totalTime/(timeVlueList.size());
 				 timeSerious.add(String.valueOf(average));
@@ -73,8 +84,25 @@ public class TestClass {
 			outputdata.add(timeSerious);
 		}
 		
+		Object[] sampleKeyset = countTop.keySet().toArray();     
+		Arrays.sort(sampleKeyset);  
+		for (Object object : sampleKeyset) {
+			ArrayList<String> topAlgorithmList=new ArrayList<String>();
+			topAlgorithmList.add(object.toString());
+			HashMap<String, Integer> sampleValue=countTop.get(object);
+			Iterator iter = sampleValue.entrySet().iterator();
+			while (iter.hasNext()) {
+				Map.Entry entry = (Map.Entry) iter.next();
+				topAlgorithmList.add(entry.getKey().toString());
+				topAlgorithmList.add(entry.getValue().toString());
+			}
+			outputdata.add(topAlgorithmList);
+		}
+		
 		String fileName="G:\\WorkSpace\\SortingAlgorithm\\result.xls";
 		new OutputExcel().outputFile(outputdata, fileName);
+		
+		// 增加一段统计排序速度在前三项的数据结构
 	}
 
 
@@ -119,7 +147,10 @@ public class TestClass {
 	}
 
 	
-	private static void TestInteger(int totalTest,HashMap<String, HashMap<Integer, ArrayList<Long>>> timeRecorder) {
+	private static void TestInteger(int totalTest,
+			HashMap<String, HashMap<Integer, ArrayList<Long>>> timeRecorder,
+			HashMap<Integer, LinkedHashMap<String, Integer>> countTop,
+			ArrayList<String> sortName) {
 		
 //		int numberSize=1000*r.nextInt(10);
 		// 当numberSize=0 时，程序运行会出先问题。 具体原因是什么不太清楚。
@@ -158,6 +189,7 @@ public class TestClass {
 //			Java中引用数据类型（包括数组，包括基本类型的数组）是传引用的。
 			
 			// 测试快速排序的时间
+			long[] times=new long[7];
 			long time1= new QuickSort<Integer>()._quickSort(numList, 0, numberSize-1);
 		    if(!timeRecorder.get("QuickSort").containsKey(numberSize)){
 		    	ArrayList<Long> list=new ArrayList<Long>();
@@ -166,10 +198,10 @@ public class TestClass {
 		    }else{
 		    	 timeRecorder.get("QuickSort").get(numberSize).add(time1);
 		    }
-		   
+		    times[0]=time1;
 		    
+		    long time2= new BubbleSort<Integer>().bubbleSort(numList2,numberSize );
 			// 测试冒泡排序
-			long time2= new BubbleSort<Integer>().bubbleSort(numList2,numberSize );
 			if(!timeRecorder.get("BubbleSort").containsKey(numberSize)){
 			    	ArrayList<Long> list=new ArrayList<Long>();
 			    	list.add(time2);
@@ -177,7 +209,7 @@ public class TestClass {
 			    }else{
 			    	timeRecorder.get("BubbleSort").get(numberSize).add(time2);
 			    }
-			    
+			 times[1]=time2;
 			// 测试堆排序
 			long time3=new HeapSort<Integer>().headSort(numList3);
 			if(!timeRecorder.get("HeapSort").containsKey(numberSize)){
@@ -187,7 +219,7 @@ public class TestClass {
 		    }else{
 		    	timeRecorder.get("HeapSort").get(numberSize).add(time3);
 		    }
-			
+			 times[2]=time3;
 		
 		
 			long time4=new InsertSort<Integer>().insertSort(numList4);
@@ -198,7 +230,7 @@ public class TestClass {
 		    }else{
 		    	timeRecorder.get("InsertSort").get(numberSize).add(time4);
 		    }
-			
+			 times[3]=time4;
 			
 			long time5=new MergeSort().mergeSort(numList5);
 			if(!timeRecorder.get("MergeSort").containsKey(numberSize)){
@@ -208,7 +240,7 @@ public class TestClass {
 		    }else{
 		    	timeRecorder.get("MergeSort").get(numberSize).add(time5);
 		    }
-			
+			 times[4]=time5;
 			
 			long time6=new ShellSort<Integer>().shellSort(numList6);
 			if(!timeRecorder.get("ShellSort").containsKey(numberSize)){
@@ -218,7 +250,7 @@ public class TestClass {
 		    }else{
 		    	timeRecorder.get("ShellSort").get(numberSize).add(time6);
 		    }
-			
+			 times[5]=time6;
 			
 			long time7=new RadixSort().radixSort(numList7);
 			if(!timeRecorder.get("RadixSort").containsKey(numberSize)){
@@ -228,10 +260,53 @@ public class TestClass {
 		    }else{
 		    	timeRecorder.get("RadixSort").get(numberSize).add(time7);
 		    }
-			
-
+			 times[6]=time7;
+			 
+             int topN=3;
+			 long[] topNtimes=new long[topN];
+			 int[] topNpositions=ChoosetopN(topN,times,topNtimes);
+			 coculateCountTop(numberSize,topNpositions,topNtimes,sortName,countTop);
+			 
 		}
 		
+	}
+
+	private static void coculateCountTop(int sampleSize,int[] topNpositions,
+			 long[] topNtimes, ArrayList<String> sortName, 
+			HashMap<Integer, LinkedHashMap<String, Integer>> countTop) {
+		for(int pos=0;pos<topNpositions.length;pos++){
+			String  sortAlgorithm=sortName.get(topNpositions[pos]);
+			if(!countTop.containsKey(sampleSize)){
+				countTop.put(sampleSize, new LinkedHashMap<String, Integer>());
+			}
+			HashMap<String, Integer> countTopValue=countTop.get(sampleSize);
+			if(countTopValue.containsKey(sortAlgorithm)){
+				int freq=countTopValue.get(sortAlgorithm);
+				countTopValue.put(sortAlgorithm, freq+1);
+			}else{
+				countTopValue.put(sortAlgorithm,1);
+			}
+		}
+		
+	}
+
+	private static int[] ChoosetopN(int n, long[] times,long[] topNTimes) {
+		int[] returnPosition=new int[n];
+		for(int i=0;i<n;i++){
+			long Min=times[0];
+			int position=0;
+			for(int j=1;j<times.length;j++){
+				if(Min>times[j]){
+					Min=times[j];
+					position=j;
+				}
+			}
+			returnPosition[i]=position;
+			topNTimes[i]=times[position];
+			times[position]=0x7fffffffffffl;
+		}
+		//printSet(" ", returnPosition);
+		return returnPosition;
 	}
 
 }
